7cb64eb6b53b5645eee2462be86b2595b0a6bc6b
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-04-26 00:06:31 +0200

    feat(package): Add exports for resume-schema and schema-primitive modules
    The functionality of the application has been enhanced by adding exports
     for the 'resume-schema' and 'schema-primitive' modules to package.json
     and src/index.ts. The 'resume-schema' module has also been refactored
     to export multiple sections of a resume, such as 'award', 'basics',
     'certificates', and more.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- src/resume-schema/index.ts
+++ src/resume-schema/index.ts
@@ -1,1 +1,15 @@
-export { Resume } from './resume.ts'
+export * from './award/award.ts';
+export * from './basics/basics.ts';
+export * from './certificates/certificates.ts';
+export * from './education/education.ts';
+export * from './interest/interest.ts';
+export * from './language/language.ts';
+export * from './location/location.ts';
+export * from './project/project.ts';
+export * from './publication/publication.ts';
+export * from './reference/reference.ts';
+export * from './skill/skill.ts';
+export * from './volunteer/volunteer.ts';
+export * from './work/work.ts';
+export * from './work/role.ts';
+export * from './resume.ts';
