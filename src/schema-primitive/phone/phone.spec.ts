ed84a2959e6dd3bc6be06a017673d6258e93706c
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-04-28 22:20:08 +0200

    refactor(schema-primitive): refactor phone number validation in Phone
    module
    
    This commit simplifies the phone number validation logic in the Phone
    module. It enforces the E.164 standard which requires phone numbers to
    be up to 15 digits long and start with '+'. Also, it updates the test
    cases to correspond with the implementation changes.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- src/schema-primitive/phone/phone.spec.ts
+++ src/schema-primitive/phone/phone.spec.ts
@@ -4,88 +4,136 @@ import { describe, expect, test } from 'vitest'
 
 import { Phone } from './phone.js'
 
-describe('Phone', () => {
-	describe('decode', () => {
-		const parse = S.decodeUnknownSync(Phone)
+describe('PhoneString', () => {
+	const parse = S.decodeUnknownSync(Phone)
 
-		const validNumbers = [
-			{ phone: '+4907121172923', description: 'German number (standard format)' },
-			{ phone: '+441632960961', description: 'UK number (standard format)' },
-			{ phone: '+353861234567', description: 'Irish number (standard format)' },
-			{
-				phone: '00353861234567',
-				description: 'Irish number with double-zero prefix (alternative format)',
-			},
-			{ phone: '+39 02 1234567', description: 'Italian number with space as separator' },
-			{
-				phone: '+1-800-123-4567',
-				description: 'US number with hyphen as separator (assuming supported)',
-			},
-			{
-				phone: '+1 800 123 4567',
-				description: 'US number with spaces (might be valid depending on logic)',
-			},
-			{
-				phone: '+49 (0) 216 554 1036',
-				description: 'German number with parentheses and leading zero',
-			},
-			{
-				phone: '+39021234567',
-				description: 'Italian number without space (might be valid depending on logic)',
-			},
-		]
+	const validNumbers = [
+		{ phone: '+4907121172923', description: 'German number (standard format)' },
+		{ phone: '+441632960961', description: 'UK number (standard format)' },
+		{ phone: '+353861234567', description: 'Irish number (standard format)' },
+		{
+			phone: '00353861234567',
+			description: 'Irish number with double-zero prefix (alternative format)',
+		},
+		{ phone: '+39 02 1234567', description: 'Italian number with space as separator' },
+		{
+			phone: '+1-800-123-4567',
+			description: 'US number with hyphen as separator (assuming supported)',
+		},
+		{
+			phone: '+1 800 123 4567',
+			description: 'US number with spaces (might be valid depending on logic)',
+		},
+		{
+			phone: '+49 (0) 216 554 1036',
+			description: 'German number with parentheses and leading zero',
+		},
+		{
+			phone: '+39021234567',
+			description: 'Italian number without space (might be valid depending on logic)',
+		},
+	]
 
-		const invalidNumbers = [
-			{ phone: '', description: 'Empty string' },
-			{ phone: ' ', description: 'Whitespace' },
-			// { phone: '+1234567890', description: 'Non-standard phone number (missing country code)' },
-			{ phone: '123456', description: 'Too short' },
-			{ phone: 'abcdefghijk', description: 'Non-numeric characters' },
-		]
+	const invalidNumbers = [
+		{ phone: '', description: 'Empty string' },
+		{ phone: ' ', description: 'Whitespace' },
+		// { phone: '+1234567890', description: 'Non-standard phone number (missing country code)' },
+		{ phone: '123456', description: 'Too short' },
+		{ phone: 'abcdefghijk', description: 'Non-numeric characters' },
+	]
 
-		test.each(validNumbers)('parse valid phone number $phone ($description)', ({ phone }) => {
-			expect(() => parse(phone)).not.toThrow()
-		})
+	test.each(validNumbers)('parse valid phone number $phone ($description)', ({ phone }) => {
+		expect(() => parse(phone)).not.toThrow()
+	})
 
-		test.each(invalidNumbers)('throws for invalid number $phone ($description)', ({ phone }) => {
-			expect(() => parse(phone)).toThrow()
-		})
+	test.each(invalidNumbers)('throws for invalid number $phone ($description)', ({ phone }) => {
+		expect(() => parse(phone)).toThrow()
 	})
 
 	describe('JSONSchema', () => {
-		test('naked', () => {
-			expect(
-				JSON.stringify(jsonSchema.make(S.encodedSchema(Phone)), null, '\t'),
-			).toMatchInlineSnapshot(`
-			"{
-				"$schema": "http://json-schema.org/draft-07/schema#",
-				"type": "string",
-				"description": "a string",
-				"title": "string"
-			}"
-		`)
+		describe('naked', () => {
+			test('with no annotations', () => {
+				expect(jsonSchema.make(Phone)).toMatchInlineSnapshot(`
+				{
+				  "$schema": "http://json-schema.org/draft-07/schema#",
+				  "description": "a phone number conforming to the E.164 format standard",
+				  "examples": [
+				    "+4907121172923",
+				    "+441632960961",
+				    "+353861234567",
+				    "00353861234567",
+				    "+39 02 1234567",
+				    "+1-800-123-4567",
+				    "+1 800 123 4567",
+				    "+49 (0) 216 554 1036",
+				  ],
+				  "pattern": "^\\+[1-9]\\d{1,14}$",
+				  "title": "PhoneString",
+				  "type": "string",
+				}
+			`)
+			})
+
+			test('with custom annotations', () => {
+				expect(
+					jsonSchema.make(
+						Phone.annotations({
+							title: 'TITLE',
+							description: 'DESCRIPTION',
+						}),
+					),
+				).toMatchInlineSnapshot(`
+					{
+					  "$schema": "http://json-schema.org/draft-07/schema#",
+					  "description": "DESCRIPTION",
+					  "examples": [
+					    "+4907121172923",
+					    "+441632960961",
+					    "+353861234567",
+					    "00353861234567",
+					    "+39 02 1234567",
+					    "+1-800-123-4567",
+					    "+1 800 123 4567",
+					    "+49 (0) 216 554 1036",
+					  ],
+					  "pattern": "^\\+[1-9]\\d{1,14}$",
+					  "title": "TITLE",
+					  "type": "string",
+					}
+				`)
+			})
 		})
 
 		test('encodedSchema', () => {
-			expect(JSON.stringify(jsonSchema.make(Phone), null, '\t')).toMatchInlineSnapshot(`
-				"{
-					"$schema": "http://json-schema.org/draft-07/schema#",
-					"type": "string",
-					"description": "Phone number that can be coveted to a valid E.164 format",
-					"title": "Phone",
-					"minLength": 1,
-					"examples": [
-						"+4907121172923",
-						"+441632960961",
-						"+353861234567",
-						"00353861234567",
-						"+39 02 1234567",
-						"+1-800-123-4567",
-						"+1 800 123 4567",
-						"+49 (0) 216 554 1036"
-					],
-					"pattern": "^\\\\+[1-9]\\\\d{1,14}$"
-				}"
+			expect(jsonSchema.make(S.encodedSchema(Phone))).toMatchInlineSnapshot(`
+				{
+				  "$schema": "http://json-schema.org/draft-07/schema#",
+				  "description": "a string",
+				  "title": "string",
+				  "type": "string",
+				}
+			`)
+		})
+
+		test('typedSchema', () => {
+			expect(jsonSchema.make(S.typeSchema(Phone))).toMatchInlineSnapshot(`
+				{
+				  "$schema": "http://json-schema.org/draft-07/schema#",
+				  "description": "a phone number conforming to the E.164 format standard",
+				  "examples": [
+				    "+4907121172923",
+				    "+441632960961",
+				    "+353861234567",
+				    "00353861234567",
+				    "+39 02 1234567",
+				    "+1-800-123-4567",
+				    "+1 800 123 4567",
+				    "+49 (0) 216 554 1036",
+				  ],
+				  "pattern": "^\\+[1-9]\\d{1,14}$",
+				  "title": "PhoneString",
+				  "type": "string",
+				}
 			`)
 		})
 	})
