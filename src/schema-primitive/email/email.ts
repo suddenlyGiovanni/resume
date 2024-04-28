280d419f96c4deb85723eb7e3b6ef3ac81012af5
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-04-29 01:37:23 +0200

    refactor(schema-primitive): refine email validation logic in schema
    The email validation logic in 'schema-primitive' has been refined. An
    'email' function is added to implement improved validation by including
    the import of 'omit' from 'trimmed-non-empty'. This new logic expects an
     Email string matching a detailed pattern and provides a more
     explanatory message in case of any issue.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- src/schema-primitive/email/email.ts
+++ src/schema-primitive/email/email.ts
@@ -1,15 +1,34 @@
+import { omit } from '../trimmed-non-empty/trimmed-non-empty.js'
 import * as S from '@effect/schema/Schema'
+
+const email =
+	<A extends string>(annotations?: S.Annotations.Filter<A>) =>
+	<I, R>(self: S.Schema<A, I, R>) => {
+		const regex = /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i
+		const pattern = regex.source
+		return self.pipe(
+			S.filter((maybeEmail): maybeEmail is A => regex.test(maybeEmail), {
+				typeId: { id: S.PatternTypeId, annotation: { regex } },
+				description: `an Email address string matching the pattern ${pattern}`,
+				message: issue =>
+					`expected an Email address string matching the pattern ${pattern}, got "${issue.actual}"`,
+				jsonSchema: {
+					format: 'email',
+					pattern,
+					...annotations?.jsonSchema,
+				},
+				examples: ['<local-part>@<domain>' as A, 'foo@bar.com' as A, 'foo.bar@baz.com' as A],
+				arbitrary: () => fc => fc.stringMatching(regex) as any,
+				...(annotations ? omit(annotations, 'jsonSchema') : {}),
+			}),
+		)
+	}
 
 export interface Email extends S.Annotable<Email, string> {}
 
-export const Email: Email = S.String.annotations({
-	identifier: 'Email',
-	title: 'email',
-	description: 'Email address',
-	examples: ['<local-part>@<domain>', 'foo@bar.com', 'foo.bar@baz.com'],
-}).pipe(
-	S.pattern(/^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i, {
-		description: 'Email address',
+export const Email: Email = S.String.pipe(
+	email({
+		identifier: 'Email',
+		title: 'Email',
 	}),
-	S.filter(_ => true, { jsonSchema: { format: 'email' } }),
 )
