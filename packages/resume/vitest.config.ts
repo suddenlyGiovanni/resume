e3f6fd487eaee7593b5fb599bf7ab5dbccc336fb
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-01-04 14:38:34 +0100

    feat(resume): add local schema and update dependencies
    Added a local `schema.json` file to the `resume` package and modified schema references accordingly. Updated Node.js TypeScript configuration to `node23` and included the new `effect` dependency in `resume` package. Removed unnecessary `publishConfig` from `resume` package settings.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ packages/resume/vitest.config.ts
@@ -0,0 +1,13 @@
+import * as process from 'node:process'
+import {defineConfig} from 'vitest/config'
+
+export default defineConfig({
+	test: {
+		reporters: process.env['GITHUB_ACTIONS'] ? ['dot', 'github-actions'] : ['default'],
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html'],
+		},
+		include: ['src/**/*.spec.ts'],
+	},
+})
