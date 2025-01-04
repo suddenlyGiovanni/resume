6de2c3ecd6ba0f3602d30e890b2deb8f4202429c
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-01-04 22:29:22 +0100

    ci(workflows): switch to Vitest for test execution
    Replaced pnpm test command with Vitest in PR checks workflow. Updated Vitest configuration to use GitHub Actions reporters conditionally, enhancing CI reporting.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
-import {defineConfig} from 'vitest/config'
+// biome-ignore lint/correctness/noNodejsModules: <explanation>
+import * as process from 'node:process'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
+		reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
 		workspace: ['packages/*'],
 	},
 })
