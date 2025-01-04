2f92db973365e653ff2f4e879142954dd715bb95
suddenlyGiovanni <suddenlyGiovanni@users.noreply.github.com>
2025-01-04 14:29:28 +0000

    chore: lint code with Biome [skip ci]

--- packages/schema-resume/vitest.config.ts
+++ packages/schema-resume/vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-
 	test: {
 		includeSource: ['./src/test/test-globals.ts'],
 		reporters: process.env['GITHUB_ACTIONS'] ? ['dot', 'github-actions'] : ['default'],
