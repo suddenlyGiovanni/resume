39df088ce104ad728688a84041b944e2c809d1de
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-06-04 13:17:30 +0200

    chore(vitest): remove unused deno plugin
    Removed the unused `@deno/vite-plugin` from the Vitest configuration.
     This cleanup optimizes the configuration by eliminating unnecessary
     dependencies.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,7 @@
 import process from 'node:process'
-
-import deno from '@deno/vite-plugin'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-	plugins: [deno()],
 	test: {
 		reporters: process.env['GITHUB_ACTIONS'] ? ['dot', 'github-actions'] : ['dot'],
 		projects: ['packages/*'],
